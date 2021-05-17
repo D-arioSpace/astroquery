a8cdfd5e7d386f71ca54917e65aee80ea70ca538
Alvaro Arroyo Parejo <carlos.arroyo@deimos-space.com>
2021-05-17 16:12:35 +0200

    adding tasks

--- test/template_test.py
+++ test/template_test.py
@@ -8,19 +8,22 @@ from datetime import datetime
 import logging
 import time
 
-
+#TODO include modes of testing
+#TODO include requests all the tabs
+#TODO include summary.log and try and except to check every set
 
 start_time = time.time()
 asteroid_set = neocc.query_list(list_name='nea_list')
 
-logging.basicConfig(filename="logephemerides.log", level=logging.INFO)
+logging.basicConfig(filename="physical_properties.log", level=logging.INFO)
 
 for asteroid in asteroid_set:
         try:
-                orbit_prop = neocc.query_object(name=asteroid, tab='ephemerides', observatory='500', start='2019-05-08 01:30', stop='2019-05-23 01:30', step='1', step_unit='days')
-                logging.info('Asteroid ' + str(asteroid) + ' get ephemerides file')
+                ast = neocc.query_object(name=asteroid, tab='physical_properties')
+                #, observatory='500', start='2019-05-08 01:30', stop='2019-05-23 01:30', step='1', step_unit='days')
+                logging.info('Object ' + str(asteroid) + ' gets physical properties')
         except:
-                logging.error('Asteroid ' + str(asteroid) + ' does NOT get ephemerides file')
+                logging.error('Object ' + str(asteroid) + ' does NOT get physical properties file')
                 continue
 
 print("--- %s seconds ---" % (time.time() - start_time))
