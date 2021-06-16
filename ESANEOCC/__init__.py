a8ba04b5e0eed278350af0cfedf30380241ab31b
Alvaro Arroyo Parejo <carlos.arroyo@deimos-space.com>
2021-06-16 11:32:28 +0200

    adding configuration as for astroquery

--- ESANEOCC/__init__.py
+++ ESANEOCC/__init__.py
@@ -1,3 +1,36 @@
-from . import lists
-from . import tabs
-from . import neocc
+"""
+
+@author: C. Álvaro Arroyo
+@contact: carlos.arroyo@deimos-space.com
+
+European Space Agency (ESA)
+
+Created on 16 Jun. 2021
+
+"""
+from astropy import config as _config
+from .core import neocc, ESAneoccClass
+
+class Conf(_config.ConfigNamespace):
+    """
+    Configuration parameters for 'ESANEOCC'
+    """
+
+    BASE_URL = _config.ConfigItem('https://neo.ssa.esa.int/'
+                                  'PSDB-portlet/download?file=')
+
+    PROPERTIES_URL = _config.ConfigItem('https://neo.ssa.esa.int/'
+                                        'search-for-asteroids?tab='
+                                        'physprops&des=')
+
+    EPHEM_URL = _config.ConfigItem('https://neo.ssa.esa.int/'
+                                   'PSDB-portlet/ephemerides?des=')
+
+    SUMMARY_URL = _config.ConfigItem('https://neo.ssa.esa.int/'
+                                     'search-for-asteroids?sum=1&des=')
+
+    TIMEOUT = 60
+
+conf = Conf()
+
+__all__ = ['neocc', 'ESAneoccClass', 'Conf', 'conf']
