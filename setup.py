7d5dc5849cebea0377412fda4d3409ffbc6e1d63
Alvaro Arroyo Parejo <carlos.arroyo@deimos-space.com>
2021-06-16 11:33:00 +0200

    changing version number and adding astropy to install_requires

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name = 'ESANEOCC',
     packages = find_packages(include=['ESANEOCC']),
-    version = '1.2',
+    version = '1.3',
     description = 'NEOCC portal Python interface',
     author = 'C. Álvaro Arroyo Parejo',
     license = 'European Space Agency',
-    install_requires = ['requests','pandas','parse','scipy','lxml','bs4'],
+    install_requires = ['requests','pandas','parse','scipy','lxml','bs4', 'astropy'],
     setup_requires = ['pytest-runner'],
     tests_require = ['pytest==4.4.1'],
     test_suite = 'test',
